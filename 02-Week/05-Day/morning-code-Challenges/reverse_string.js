b3830d96bd9051618a64b3cfbb9926fb24caaa64
Ken Carrington <kcarring93@verizon.net>
2015-10-13 09:33:08 -0700

    mostly first h/w and project1-initial

--- 02-Week/05-Day/morning-code-Challenges/reverse_string.js
+++ 02-Week/05-Day/morning-code-Challenges/reverse_string.js
@@ -1,4 +1,4 @@
- function myReverse (str) {
+/* function myReverse (str) {
 	var arr = str.split('');
 	for (var i=0; i<arr.length/2; i++) {
 		var temp = arr[i];
@@ -9,4 +9,18 @@
 }
 
 console.log(myReverse("String"));
-console.log(myReverse("abcde"));
+console.log(myReverse("abcde"));*/
+
+//prototype adds a property(a key/value pair - in this case a key:function) to the object
+String.prototype.myReverse = function () {
+	var arr = this.split('');
+	for (var i=0; i<arr.length/2; i++) {
+		var temp = arr[i];
+		arr[i] = arr[arr.length-1-i];
+		arr[arr.length-1-i] = temp;
+	}
+	return arr.join('');
+}
+
+console.log("String".myReverse());
+console.log("abcde".myReverse());
---
+++ 03-Week/project1/load_flash_card_data.js
@@ -0,0 +1,129 @@
+var JLPT_Kanji_N5 = [];
+JLPT_Kanji_N5[0] = ['<span class=""kanji"">口</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">く</span></td><br><td><span class=""kun"">くち</span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>mouth</b>'];
+JLPT_Kanji_N5[1] = ['<span class=""kanji"">上</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">じょう</span></td><br><td><span class=""kun"">うえ<div>あ</div><div>のぼ</div></span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>above, up, over</b>'];
+JLPT_Kanji_N5[2] = ['<span class=""kanji"">入</span><br><br><br><br><br><br>', '<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">にゅう</span></td><br><td><span class=""kun"">はい<div>い</div></span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>enter</b>'];
+JLPT_Kanji_N5[3] = ['<span class=""kanji"">川</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on""></span></td><br><td><span class=""kun"">かわ</span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>river</b>'];
+JLPT_Kanji_N5[4] = ['<span class=""kanji"">七</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">しち</span></td><br><td><span class=""kun"">なな<div>なの</div></span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>seven</b>'];
+JLPT_Kanji_N5[5] = ['<span class=""kanji"">二</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">に</span></td><br><td><span class=""kun"">ふた<div>ふつ</div></span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>two</b>'];
+JLPT_Kanji_N5[6] = ['<span class=""kanji"">三</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">さん</span></td><br><td><span class=""kun"">み<br />みっ</span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>three</b>'];
+JLPT_Kanji_N5[7] = ['<span class=""kanji"">山</span><br><br><br><br><br><br>', 
+'<div class=""conjug""><br><table><br><tr><br><th></th><br><th><span class=""on"">音読み</th><br><th><span class=""kun"">訓読み</th><br></tr><br><br><tr><br><th></th><br><td><span class=""on"">さん</span></td><br><td><span class=""kun"">やま</span></td><br></tr><br><br><tr><br><th><br><br></table></div><br><br><b>mountain</b>'];
+
+/*JLPT_Kanji_N5[8] = ['', ''];
+JLPT_Kanji_N5[9] = ['', ''];
+JLPT_Kanji_N5[10] = ['', ''];
+JLPT_Kanji_N5[11] = ['', ''];
+JLPT_Kanji_N5[12] = ['', ''];
+JLPT_Kanji_N5[13] = ['', ''];
+JLPT_Kanji_N5[14] = ['', ''];
+JLPT_Kanji_N5[15] = ['', ''];*/
+
+// The JLPT_N5_Genki had audio and img, and no way to delimit sides
+/* This one has too many tags
+var Kanji_JLPT_N4 = [];
+Kanji_JLPT_N4[0] = ['<div style='font-family: Arial; font-size: 30px;'>近</div><br><br><span class=""hint"">On Read:<br><br><a class=hint href=""#""<br>onclick=""this.style.display='none';document.getElementById('hint310037608').style.display='block';return false;""><br>Show On Read</a><div id=""hint310037608"" class=hint style=""display: none"">キン</div><br></span><br><br><span class=""hint"">Kun Read:<br><br><a class=hint href=""#""<br>onclick=""this.style.display='none';document.getElementById('hint309363584').style.display='block';return false;""><br>Show Kun Read</a><div id=""hint309363584"" class=hint style=""display: none"">ちか（い）</div><br></span><br><br><br>', 
+'<div style='font-family: Arial; font-size: 20px;'>Close, soon</div><br><br><br><div style='font-family: Arial; font-size: 12px;'>On Read: キン[]</div><br><div style='font-family: Arial; font-size: 12px;'>Kun Read: ちか（い）[]</div><br><br><div style='font-family: Arial; font-size: 10px;color:black'>Grade 2</div><br><br><div style='font-family: Arial; font-size: 10px;'><img src=""chikai.gif"" /></div><br><div style='font-family: Arial; font-size: 10px;'></div><br><br><br><span class=""hint"">Mnemotechnic Verse:<br><br></span><br><br>'];
+Kanji_JLPT_N4[1] = ['<div style='font-family: Arial; font-size: 30px;'>英</div><br><br><span class=""hint"">On Read:<br><br><a class=hint href=""#""<br>onclick=""this.style.display='none';document.getElementById('hint291566320').style.display='block';return false;""><br>Show On Read</a><div id=""hint291566320"" class=hint style=""display: none"">エイ</div><br></span><br><br><span class=""hint"">Kun Read:<br><br><a class=hint href=""#""<br>onclick=""this.style.display='none';document.getElementById('hint409266704').style.display='block';return false;""><br>Show Kun Read</a><div id=""hint409266704"" class=hint style=""display: none"">ー</div><br></span><br><br><br>', 
+'<div style='font-family: Arial; font-size: 20px;'>English, England</div><br><br><br><div style='font-family: Arial; font-size: 12px;'>On Read: エイ[]</div><br><div style='font-family: Arial; font-size: 12px;'>Kun Read: ー[]</div><br><br><div style='font-family: Arial; font-size: 10px;color:black'>Grade 4</div><br><br><div style='font-family: Arial; font-size: 10px;'><img src=""ei.gif"" /></div><br><div style='font-family: Arial; font-size: 10px;'></div><br><br><br><span class=""hint"">Mnemotechnic Verse:<br><br></span><br><br>'];
+Kanji_JLPT_N4[2] = ['', ''];
+Kanji_JLPT_N4[3] = ['', ''];
+Kanji_JLPT_N4[4] = ['', ''];
+Kanji_JLPT_N4[5] = ['', ''];
+Kanji_JLPT_N4[6] = ['', ''];
+Kanji_JLPT_N4[7] = ['', ''];
+
+Kanji_JLPT_N4[8] = ['', ''];
+Kanji_JLPT_N4[9] = ['', ''];
+Kanji_JLPT_N4[10] = ['', ''];
+Kanji_JLPT_N4[11] = ['', ''];
+Kanji_JLPT_N4[12] = ['', ''];
+Kanji_JLPT_N4[13] = ['', ''];
+Kanji_JLPT_N4[14] = ['', ''];
+Kanji_JLPT_N4[15] = ['', '']; */
+
+//var Kens_JTimes = [];
+
+var Javascript1_cards = [];
+Javascript1_cards[0] = ['this', 'Refers to the function\'s execution context'];
+Javascript1_cards[1] = ['function', 'Defines a function expression'];
+Javascript1_cards[2] = ['[]', ' Array initializer/literal syntax.'];
+Javascript1_cards[3] = ['{}', 'Object initializer/literal syntax.'];
+Javascript1_cards[4] = ['/ab+c/i', 'Regular expression literal syntax.'];
+Javascript1_cards[5] = ['( ) ', 'Grouping operator.'];
+Javascript1_cards[6] = ['Property accessors ', '<div>Member operators provide access to a property or method of an object</div><div>(object.property and object[""property""]).</div>'];
+Javascript1_cards[7] = ['new', 'Creates an instance of a constructor.'];
+
+/*Javascript1_cards[8] = ['', ''];
+Javascript1_cards[9] = ['', ''];
+Javascript1_cards[10] = ['', ''];
+Javascript1_cards[11] = ['', ''];
+Javascript1_cards[12] = ['', ''];
+Javascript1_cards[13] = ['', ''];
+Javascript1_cards[14] = ['', ''];
+Javascript1_cards[15] = ['', ''];*/
+
+console.log(Javascript1_cards[0]);
+
+
+var Computer_Sci = [];
+Computer_Sci[0] = ['<span class=""tags"">Linux</span><br /><br /><br>todo.txt:&nbsp;A <span class=cloze>[...]</span> in your todo.txt file represents a single task.', 
+'<span class=""tags"">Linux</span><br /><br /><br>todo.txt:&nbsp;A <span class=cloze>single line</span> in your todo.txt file represents a single task.<br><br><br><div class=""extra""></div>'];
+Computer_Sci[1] = ['<span class=""tags"">Linux</span><br /><br /><br>Linux: ps stands for <span class=cloze>[...]</span>.', 
+'<span class=""tags"">Linux</span><br /><br /><br>Linux: ps stands for <span class=cloze>process status</span>.<br><br><br><div class=""extra""></div>'];
+Computer_Sci[2] = ['What does grep stand for? ', 
+'<b>G</b>lobally search a <b>r</b>egular <b>e</b>xpression and <b>p</b>rint.'];
+Computer_Sci[3] = ['What is dpkg?', 
+'A package manager for <i>Debian</i> based systems.'];
+Computer_Sci[4] = ['What command tells you the directory you\'re currently in?', 
+'pwd'];
+Computer_Sci[5] = ['What is an absolute path?', 
+'A path that begins in the root directory.<div><br /></div><div>Example: /etc/usr/share/X11</div>'];
+Computer_Sci[6] = ['What is a relative path? ', 
+'A path that begins in the current directory.'];
+Computer_Sci[7] = ['What button autocompletes a command?', 
+'Tab'];
+
+console.log(Computer_Sci[0]);
+
+/*Computer_Sci[8] = ['', ''];
+Computer_Sci[9] = ['', ''];
+Computer_Sci[10] = ['', ''];
+Computer_Sci[11] = ['', ''];
+Computer_Sci[12] = ['', ''];
+Computer_Sci[13] = ['', ''];
+Computer_Sci[14] = ['', ''];
+Computer_Sci[15] = ['', '']; */
+
+var JS2_Sivers = [];
+JS2_Sivers[0] = ['What does document.write do?', 
+'document.write(str) takes a string of text and displays it in the browser, right where the &lt;script&gt; tag is located.'];
+JS2_Sivers[1] = ['When people say HTML5, what do they really mean? ', 
+'HTML5 markup + JavaScript APIs + CSS(3) = &quot;HTML5&quot;'];
+JS2_Sivers[2] = ['internal model of the HTML page is called the ___', 
+' internal model of the HTML page is called the DOM'];
+JS2_Sivers[3] = ['Variables must start with a ______, __________, or ___________.', 
+'Variables must start with a letter, underscore, or dollar sign.'];
+JS2_Sivers[4] = ['Why not run JavaScript in the &lt;head&gt;?', 
+'Because the DOM hasn\'t been created yet.<br><br />'];
+JS2_Sivers[5] = ['How to take JavaScript from the &lt;head&gt; but make it run after the page is loaded?', 
+'function init() {<br>&nbsp;&nbsp;// stuff here<br>}<br>window.onload = init;<br><br />'];
+JS2_Sivers[6] = ['You have an array arr, but don\'t know the length. Get a random element from it in 2 lines.', 
+'randomIndex = Math.floor(Math.random() * arr.length);<br />myValue = arr[randomIndex];<br /><br />'];
+JS2_Sivers[7] = ['You define a function with __________. You call a function with _________.<br><br /> ', 
+'You define a function with parameters. You call a function with arguments.<br><br />'];
+
+/*JS2_Sivers[8] = ['', ''];
+JS2_Sivers[9] = ['', ''];
+JS2_Sivers[10] = ['', ''];
+JS2_Sivers[11] = ['', ''];
+JS2_Sivers[12] = ['', ''];
+JS2_Sivers[13] = ['', ''];
+JS2_Sivers[14] = ['', ''];
+JS2_Sivers[15] = ['', ''];*/
+
